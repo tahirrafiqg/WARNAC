cb5bc0d2282de918c8aada50430661d9b6b8b6a8
Tahir Rafiq <tahirrafiqg@gmail.com>
2025-06-25 21:42:30 +0500

    Initial setup

---
+++ src/components/Header.tsx
@@ -0,0 +1,57 @@
+import { Alert, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import VectorIcon from '../utils/VectorIcon';
+import { Colors } from '../theme/Colors';
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Logo}>WARNAC</Text>
+      <View style={styles.iconContainer}>
+        <VectorIcon
+          type="Ionicons"
+          name="camera-outline"
+          color={Colors.secondaryColor}
+                  size={30}
+                  onPress={() => Alert.alert('Camera Pressed')}
+        />
+        <VectorIcon
+          type="Ionicons"
+          name="search"
+          color={Colors.secondaryColor}
+          size={25}
+        />
+        <VectorIcon
+          type="Entypo"
+          name="dots-three-vertical"
+          color={Colors.secondaryColor}
+          size={25}
+        />
+      </View>
+    </View>
+  );
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+    container: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 10,
+    backgroundColor: Colors.primaryColor,
+  },
+  Logo: {
+    color: Colors.white,
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+    iconContainer: {
+      flexDirection: 'row',
+        gap: 10,
+      alignItems: 'center',
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
 import React from 'react';
 import VectorIcon from '../utils/VectorIcon';
 import { Colors } from '../theme/Colors';
+import Header from '../components/Header';
 
 const HomeScreen = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>HomeScreen</Text>
-      <VectorIcon type="Ionicons" name="ios-person" color={Colors.secondaryColor} size={30} />
-    </View>
+    <SafeAreaProvider>
+      <StatusBar
+        animated={false}
+        backgroundColor="black"
+        hidden={false}
+      />
+      <SafeAreaView style={styles.container}>
+        <Header />
+        <Text style={styles.text}>HomeScreen</Text>
+        <VectorIcon
+          type="Ionicons"
+          name="ios-person"
+          color={Colors.secondaryColor}
+          size={30}
+        />
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 };
 
@@ -16,13 +32,12 @@ export default HomeScreen;
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: Colors.background,
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     },
     text: {
     fontSize: 20,
-    color: 'black',
+    color: Colors.textColor,
     fontWeight: 'bold',
     }
 });
